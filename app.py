2a416b6e0154e12dda623af7bf1629f948353648
Alvin Kang <oneinallin1@gmail.com>
2018-07-12 10:51:44 -0700

    Implement post method for User class

--- app.py
+++ app.py
@@ -29,3 +29,22 @@ class User(Resource):
             if (name == users["name"]):
                 return user, 200
         return "User not found", 404
+
+    def post(self, name):
+        parser = reqparse.RequestParser
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if (name == users["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+
+        users.append(user)
+        return user, 201
