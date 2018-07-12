0faa9c166d41298adc3036b79d06e39d53520e80
Alvin Kang <oneinallin1@gmail.com>
2018-07-12 10:46:05 -0700

    Create user model

--- app.py
+++ app.py
@@ -3,3 +3,21 @@ from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
+
+users = [
+    {
+        "name": "Nick",
+        "age": 42,
+        "occupation": "Network Engineer"
+    },
+    {
+        "name": "Elvin",
+        "age": 32,
+        "occupation": "Doctor"
+    },
+    {
+        "name": "Jass",
+        "age": 22,
+        "occupation": "Web Developer"
+    }
+]
